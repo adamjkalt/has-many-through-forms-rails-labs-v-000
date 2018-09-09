dc31417d70754a2da890b7c66e02d1bb74b0df48
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-09 19:45:53 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user_comments = @user.comments.select("distinct post_id").map{|user| user.post}
+    @user_posts = @user.comments.select("distinct post_id").map{|user| user.post}
   end
 
 end
